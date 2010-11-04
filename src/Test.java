62352c992d558d39d8e891f377089602d5756880
Dean Sheppard <sheppard.dean@gmail.com>
2010-11-03 20:27:11 -0400

    Initial Config

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: dean
+ * Date: Nov 3, 2010
+ * Time: 8:20:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+
+        System.out.println("Test");
+    }
+}
